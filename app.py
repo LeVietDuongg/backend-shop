8c4f35ef0d36011d8f19919fb3c133132e36c955
LeVietDuongg <22520300@gmail.com>
2024-11-30 00:32:38 +0700

    d

--- app.py
+++ app.py
@@ -28,7 +28,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    last_login = db.Column(db.DateTime, nullable=True)
 
+# Cập nhật last_login khi đăng nhập thành công
+User.last_login = datetime.datetime.utcnow()
+db.session.commit()
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
